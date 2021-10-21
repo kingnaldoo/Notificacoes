34fa44ee283e11e42ee17587ffd9f9581dbbdaf0
kingnaldoo <reinaldosn2016@gmail.com>
2021-10-21 18:53:15 -0300

    add dscegov

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import dscegov from 'cegov-antd';
+import 'cegov-antd/dist/index.css';
+
+import pt_BR from 'antd/lib/locale/pt_BR';
+
+import type { AppProps } from 'next/app';
+import { ConfigProvider } from 'antd';
+
+const { Header, Acessibility } = dscegov();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+
+  return (
+    <ConfigProvider locale={pt_BR}>
+      <Acessibility accessibility/>
+      <Component {...pageProps} />;
+    </ConfigProvider>
+  );
 }
-export default MyApp
+export default MyApp;
