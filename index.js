04591356e96f082a47683f89c89bc1d5c5fad828
kingnaldoo <reinaldosn2016@gmail.com>
2021-10-21 17:01:37 -0300

    add parse-dashboard

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const { default: ParseServer } = require('parse-server');
+const ParseDashboard = require(('parse-dashboard'));
 
 const app = express();
 
@@ -12,6 +13,18 @@ const parseServer = new ParseServer({
     publicServerURL: 'http://localhost:1337/parse'
 });
 
+const dashboard = new ParseDashboard({
+    apps: [
+        {
+            serverURL: 'http://localhost:1337/parse',
+            appId: 'notificacoes',
+            masterKey: '12345678',
+            appName: 'Notificacoes'
+        }
+    ]
+})
+
 app.use('/parse', parseServer.app);
+app.use('/dashboard', dashboard)
 
 app.listen(1337, () => { console.log('Rest api parse rodando em http://localhost:1337/parse')})
